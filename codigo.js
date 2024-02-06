28f8934538420eecaed0c7b053508f3c25b7f6e2
Daniel Olaverria <wilolav76@hotmail.com>
2024-02-05 23:06:48 -0400

    Segunda subida, corregido JS

--- codigo.js
+++ codigo.js
@@ -3,9 +3,10 @@ let botoncompartirdesktop = document.querySelector(".img_boton_compartir_desktop
 let secciongeneral = document.querySelector(".seccion_general");
 let redessociales = document.querySelector(".redes_sociales");
 let nombreavatar = document.querySelector(".contenedor_avatar");
-
+redessociales.style.display="none";
+nombreavatar.style.display="flex";
 botoncompartirmovil.addEventListener("click", () => {
-    if ("secciongeneral.display.width>=0.88*100" && "secciongeneral.display.width<0.64*100"){
+    if ("secciongeneral.display.width>=0.20*100" && "secciongeneral.display.width<0.64*100"){
         redessociales.style.display="flex";
         nombreavatar.style.display="none";
     } else{
@@ -15,7 +16,7 @@ botoncompartirmovil.addEventListener("click", () => {
 });
 
 botoncompartirdesktop.addEventListener("click", () => {
- 
+   
     if ("secciongeneral.display.width>=0.65*100"){
         nombreavatar.style.display="flex";
         redessociales.style.display="none";
